4c4bfa346f66869aabc3fcbc2e45158d38fcce74
wisdis <tovkacvlad09@gmail.com>
2026-03-18 20:52:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import discord
 from discord.ext import commands
 import os
+import random
 
-# Берём токен из Environment Variable на Railway
+# токен
 TOKEN = os.getenv("TOKEN")
 
-# Создаём intents
+# intents
 intents = discord.Intents.default()
 intents.message_content = True  # важно для работы команд
 
-# Создаём бота с префиксом "." и intents
-bot = commands.Bot(command_prefix=".", intents=intents, help_command=None)
+# Префикс и intents
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -18,13 +19,12 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Привет!")
-
-@bot.command()
-async def help(ctx):
-     await ctx.send("""
-     **.hello** - *приветствие от бота*
-     **.help** - *выводит список команд*
-     """)
-
+    fraza = ["Привет!"
+        "Приветствую!"
+        "Добрый день 👋"
+        "Здравствуйте!"
+        "Салют!"
+        "Хай!"]
+    await ctx.send(choice.random(privet))
+    
 bot.run(TOKEN)

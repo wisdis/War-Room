c8b8ce439918e28e8b9dda9b61db1cceda08b5fe
wisdis <tovkacvlad09@gmail.com>
2026-03-18 23:28:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    fraza = [
+    privet = [
         "Привет!"
         "Приветствую!"
         "Добрый день 👋"
@@ -28,5 +28,73 @@ async def hello(ctx):
         "Хай!"
     ]
     await ctx.send(choice.random(privet))
-    
+
+async def give_income():
+    while True:
+        for member in guild.members:
+            income = 0
+
+            # доход от ролей
+            for role in member.roles:
+                if role.id in role_income:
+                    income += role_income[role.id]
+
+            # доход от предметов
+            user_items = get_user_items(member.id)
+            for item in user_items:
+                income += item["income_bonus"]
+
+            add_money(member.id, income)
+
+        await asyncio.sleep(60)  # каждая минута
+
+@bot.command()
+async def additem(ctx):
+    await ctx.send("Название предмета?")
+    name = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+
+    await ctx.send("Цена?")
+    price = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+
+    await ctx.send("Описание или Skip")
+    desc = await bot.wait_for("message", check=lambda m: m.author == ctx.author)
+
+    item = {
+        "название": name.content,
+        "цена": int(price.content),
+        "описание": None if desc.content == "Skip" else desc.content,
+        "разрешённая роль": [],
+        "запрещённая роль": [],
+        "баффы": {},
+        "дебаффы": {}
+    }
+
+    shop.append(item)
+    await ctx.send("Предмет создан")
+
+
+def can_buy(member, item):
+    if item["allowed_roles"]:
+        if not any(role.id in item["allowed_roles"] for role in member.roles):
+            return False
+
+    if any(role.id in item["blocked_roles"] for role in member.roles):
+        return False
+
+    return True
+
+role_income = {
+    role_id: {
+        "Доход": 10,
+        "Население": 2,
+        "Стабильность": -1
+    }
+}
+{
+    "Название": "Фабрика",
+    "Цена": 1000,
+    "Баффы": {"Доход": 20},
+    "Дебафф": {"Стабильность": -5}
+}
+
 bot.run(TOKEN)

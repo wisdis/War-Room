eb2d6a87ce5a5d50f35868f0be128889fdffa3fb
wisdis <tovkacvlad09@gmail.com>
2026-03-19 01:01:34 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,19 +1,23 @@
-user_money = {}
-user_items_db = {}
-shop = []
+# database.py
+shop = []  # список всех предметов
+user_items = {}  # предметы каждого пользователя
+user_balance = {}  # баланс каждого пользователя
 
 def add_money(user_id, amount):
-    """Добавить деньги пользователю"""
-    if user_id not in user_money:
-        user_money[user_id] = 0
-    user_money[user_id] += amount
+    if user_id not in user_balance:
+        user_balance[user_id] = 0
+    user_balance[user_id] += amount
+
+def get_balance(user_id):
+    return user_balance.get(user_id, 0)
 
 def get_user_items(user_id):
-    """Получить предметы пользователя"""
-    if user_id not in user_items_db:
-        user_items_db[user_id] = []
-    return user_items_db[user_id]
+    return user_items.get(user_id, [])
 
-def get_user_money(user_id):
-    """Получить деньги пользователя"""
-    return user_money.get(user_id, 0)
+def apply_item_effects(user_id, item):
+    if user_id not in user_items:
+        user_items[user_id] = []
+    user_items[user_id].append(item)
+    
+    # баффы/дебаффы можно хранить или применять
+    # например, если хочешь хранить общий доход от предметов, то пока просто добавляем их в список
